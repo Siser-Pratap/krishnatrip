282a2801fd8df12db111d659255020b0ab2c3c4d
Siser Pratap <siserinsevoc@gmail.com>
2024-10-12 18:44:03 +0530

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div className="bg-[url('/images.jpg')] bg-cover h-[95vh] mt-5 w-[100vw]">
+    <div className="bg-[url('/images.jpg')] bg-cover h-[100vh]  w-[100vw]">
     <Navbar />
     <Header />
     </div>
