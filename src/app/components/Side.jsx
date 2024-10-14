9b50b107739bda0c2eb4ddfca880506487ea37fa
Siser Pratap <siserinsevoc@gmail.com>
2024-10-14 14:34:55 +0530

    added sub section

--- src/app/components/Side.jsx
+++ src/app/components/Side.jsx
@@ -5,32 +5,14 @@ const Side = () => {
   return (
     <div className='h-[15vh] min-h-[15vh] flex justify-evenly overflow-auto bg-[#252525] items-center w-[100vw] gap-[1rem]'>
         {sideLinks.map((sideLinks, index)=>(
-          <div className='flex items-center '>
-            <div key={index} className='flex flex-col justify-center w-[10vw] items-center max-h-[10vh]  gap-[0.2rem]'>
-                {/* <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/bungee-jumping.png" alt="bungee-jumping"/> */}
+          <div className='flex items-center justify-between ' key={index}>
+            <div className='flex flex-col justify-center w-[10vw] items-center max-h-[10vh]  gap-[0.2rem]'>
                 {sideLinks.src}
                 <p className='text-[1.5rem] font-[sideFont] text-gray-300'>{sideLinks.name}</p> 
              </div>
              <hr className='w-[0.1rem] h-[5rem] bg-gray-950 border-none'></hr>
           </div>
-            //  <d></d>
           ))}
-          
-             
-             {/* <hr class="vertical-hr">
-
-<style>
-  .vertical-hr {
-    width: 2px;
-    height: 100px;
-    background-color: black;
-    border: none;
-    transform: rotate(90deg);
-    margin: 0 10px;
-  }
-</style> */}
-
-        
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,29 @@ import Header from "./components/Header";
 import Side from "./components/Side";
 export default function Home() {
   return (
+    <>
     <div className="relative h-[100vh] w-full overflow-hidden">
       <img className="absolute inset-0 z-[-1] opacity-70 object-cover w-full h-full " src="/images.jpg" />
       <div className="relative z-[10] h-full w-full ">
           <Navbar />
           <Header  />
           <Side />
+      </div>
     </div>
+    <div className="h-[20vh] w-full flex justify-center flex-col gap-[2rem] mt-32 items-center overflow-visible">
+      <h1>THE ADVENTURE</h1>
+      <p className="italic">Mastering the art of perfect adventure for 10+ years in the wild.</p>
+      {/* <hr className="w-[60px] m-[2rem] border-none h-[60px]"></hr> */}
+      <svg className="h-[20vh] w-full bg-red-800">
+            <path d="M 10 100 Q 250 100 490 100" stroke="white" fill="transparent"/>
+      </svg>
     </div>
+    <div>
+      <div className="bg-red h-[100px] w-[500px]"></div>
+    </div>
+    
+    </>
+    
     
   );
 }
