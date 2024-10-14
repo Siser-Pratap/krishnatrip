53cda3821855b568ff9363712f9a641877a61abe
Siser Pratap <siserinsevoc@gmail.com>
2024-10-14 10:48:48 +0530

    updated index

--- src/app/components/Side.jsx
+++ src/app/components/Side.jsx
@@ -6,7 +6,7 @@ const Side = () => {
     <div className='h-[15vh] min-h-[15vh] flex justify-evenly overflow-auto bg-[#252525] items-center w-[100vw] gap-[1rem]'>
         {sideLinks.map((sideLinks, index)=>(
           <div className='flex items-center '>
-            <div className='flex flex-col justify-center w-[10vw] items-center max-h-[10vh]  gap-[0.2rem]'>
+            <div key={index} className='flex flex-col justify-center w-[10vw] items-center max-h-[10vh]  gap-[0.2rem]'>
                 {/* <img width="35" height="35" src="https://img.icons8.com/ios-filled/50/bungee-jumping.png" alt="bungee-jumping"/> */}
                 {sideLinks.src}
                 <p className='text-[1.5rem] font-[sideFont] text-gray-300'>{sideLinks.name}</p> 
